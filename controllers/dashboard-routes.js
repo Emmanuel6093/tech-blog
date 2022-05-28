fec4c11a518f7adee95647c7d7cd2a41c16ceb83
Emmanuel6093 <elorenzo6093@gmail.com>
2022-05-28 13:57:47 -0400

    changed dashboard routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,53 +1,83 @@
 const router = require('express').Router();
-const { Post } = require('../models/');
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
-  try {
-    // store the results of the db query in a variable called postData. should use something that "finds all" from the Post model. may need a where clause!
-
-    // this sanitizes the data we just got from the db above (you have to create the above)
-    const posts = postData.map((post) => post.get({ plain: true }));
-
-    // fill in the view to be rendered
-    res.render('hmmmm what goes here', {
-      // this is how we specify a different layout other than main! no change needed
-      layout: 'dashboard',
-      // coming from line 10 above, no change needed
-      posts,
-    });
-  } catch (err) {
-    res.redirect('login');
-  }
-});
-
-router.get('/new', withAuth, (req, res) => {
-  // what view should we send the client when they want to create a new-post? (change this next line)
-  res.render('hmmmm what goes here', {
-    // again, rendering with a different layout than main! no change needed
-    layout: 'dashboard',
-  });
-});
-
-router.get('/edit/:id', withAuth, async (req, res) => {
-  try {
-    // what should we pass here? we need to get some data passed via the request body
-    const postData = await Post.findByPk(????);
-
-    if (postData) {
-      // serializing the data
-      const post = postData.get({ plain: true });
-      // which view should we render if we want to edit a post?
-      res.render('hmmmm what goes here', {
-        layout: 'dashboard',
-        post,
+// get all posts for dashboard
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+      where: {
+        user_id: req.session.user_id
+      },
+      attributes: [
+        'id',
+        'post_text',
+        'title',
+        'created_at'
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+            model: User,
+            attributes: ['username']
+          }
+        ]
+      })
+      .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('dashboard', { posts, loggedIn: true, username: req.session.username });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
       });
-    } else {
-      res.status(404).end();
-    }
-  } catch (err) {
-    res.redirect('login');
-  }
-});
+  });
+  router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findByPk(req.params.id, {
+      attributes: [
+        'id',
+        'post_text',
+        'title',
+        'created_at'
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (dbPostData) {
+          const post = dbPostData.get({ plain: true });
 
-module.exports = router;
+          res.render('edit-post', {
+            post,
+            loggedIn: true,
+            username: req.session.username
+          });
+        } else {
+          res.status(404).end();
+        }
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+
+  module.exports = router; 
