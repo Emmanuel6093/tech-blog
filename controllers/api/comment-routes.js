c00ed9f61254132292f44dc4b365011ae5693014
Emmanuel6093 <elorenzo6093@gmail.com>
2022-05-29 13:48:07 -0400

    updated home comment route

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,17 +1,50 @@
 const router = require('express').Router();
-const { Comment } = require('../../models/');
+const { response } = require('express');
+const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
+const { route } = require('../dashboard-routes');
 
-router.post('/', withAuth, async (req, res) => {
-  try {
-    const newComment = await Comment.create({
-      ...req.body,
-      userId: req.session.userId,
-    });
-    res.json(newComment);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+route.get('/', withAuth, (req,res) => {
+    Comment.finAll ()
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).js
+    })
 });
 
-module.exports = router;
+router.post('/', withAuth, (req, res) => {
+    if (req.session) {
+      Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.session.user_id,
+        post_id: req.body.post_id
+      })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
+        });
+    }
+  });
+
+router.delete('/:id', withAuth, (req, res) => {
+    Comment.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbCommentData => {
+        if (!dbCommentData) {
+          res.status(404).json({ message: 'No comment found with this id' });
+          return;
+        }
+        res.json(dbCommentData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  module.exports = router; 
